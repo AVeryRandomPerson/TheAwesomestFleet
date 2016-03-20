b670010176b89c8aa302887f0e7e9470e461deb0
AVeryRandomPerson <noobie___O_O@live.co.uk>
2016-03-21 01:12:45 +0800

    Created the base main. Nothing but print at the moment.

---
+++ TheAwesomestFleet/src/TheAwesomestFleet.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : TheAwesomestFleet.cpp
+// Author      : Jon
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include<cstdlib>
+#include<cstdio>
+
+using namespace std;
+
+int main() {
+	printf("This will soon become a simulation program of space racing !");
+
+
+	return 0;
+}
